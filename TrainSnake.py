07415c875b3465fa8fc73f3ed0f9f3a2fef25848
SreeLasya3103 <84137718+SreeLasya3103@users.noreply.github.com>
2024-02-08 21:39:51 -0500

    Update TrainSnake.py

--- TrainSnake.py
+++ TrainSnake.py
@@ -82,3 +82,54 @@ class DDQNAgent:
 
     def save(self, name):
         torch.save(self.target_model.state_dict(), name)
+
+env = Snake()
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+agent = DDQNAgent(state_size, action_size)
+batch_size = 64
+EPISODES = 50000
+reward_list_training = []
+apple_list_training = []
+timestep_list_training = []
+eps_list = []
+max_apple_count = 1
+reached = False
+
+for e in range(EPISODES):
+    state = env.reset()
+    state = np.reshape(state, [1, state_size])
+    done = False
+    episode_reward = 0
+    timestep = 0
+    
+    while(not done):
+        action = agent.act(state)
+        next_state, reward, done, info = env.step(action)
+        next_state = np.reshape(next_state, [1, state_size])
+        agent.remember(state, action, reward, next_state, done)
+        state = next_state
+        episode_reward += reward
+        timestep+=1
+        if done:
+            if e % 1000 == 0:
+                print("Episode: {}/{}, Episode Reward: {}, Epsilon: {:.2}, Episode Apple Count: {}, Timestep: {}"
+                      .format(e, EPISODES, episode_reward, agent.epsilon, env.apple_count, timestep))
+            reward_list_training.append(episode_reward)
+            apple_list_training.append(env.apple_count)
+            timestep_list_training.append(timestep)
+            # Saving weights
+            agent.save("billodal_syadavil_akhilshr_project_ddqn_1.h5")
+            break
+    if len(agent.memory) > batch_size:
+        agent.replay()
+    
+    eps_list.append(agent.epsilon)
+    if len(agent.memory) > 60000:
+        if not reached: # THIS WILL RUN ONLY ONCE
+            train_start_at = e
+            reached = True
+        if agent.epsilon > agent.epsilon_min:
+            agent.epsilon = agent.epsilon*((agent.epsilon_min/1)**(1/(EPISODES-train_start_at)))
+
+env.close()
