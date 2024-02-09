5f60b981808b9b79c8083941c7fce7f52e72a0cc
SreeLasya3103 <84137718+SreeLasya3103@users.noreply.github.com>
2024-02-08 21:45:16 -0500

    Update TrainSnake.py

--- TrainSnake.py
+++ TrainSnake.py
@@ -96,40 +96,66 @@ eps_list = []
 max_apple_count = 1
 reached = False
 
+
+
+
+
+
+
+import wandb
+
+# Initialize WandB
+wandb.init(project="SNAKE_FINAL", name="Final_Run")
+
+# Begin training loop
 for e in range(EPISODES):
     state = env.reset()
     state = np.reshape(state, [1, state_size])
     done = False
     episode_reward = 0
     timestep = 0
-    
-    while(not done):
+
+    while not done:
         action = agent.act(state)
         next_state, reward, done, info = env.step(action)
         next_state = np.reshape(next_state, [1, state_size])
         agent.remember(state, action, reward, next_state, done)
         state = next_state
         episode_reward += reward
-        timestep+=1
+        timestep += 1
+
         if done:
+            # Log metrics to WandB
+            wandb.log({
+                "Episode Reward": episode_reward,
+                "Epsilon": agent.epsilon,
+                "Episode Apple Count": env.apple_count,
+                "Timestep": timestep
+            })
+
             if e % 1000 == 0:
                 print("Episode: {}/{}, Episode Reward: {}, Epsilon: {:.2}, Episode Apple Count: {}, Timestep: {}"
                       .format(e, EPISODES, episode_reward, agent.epsilon, env.apple_count, timestep))
+
             reward_list_training.append(episode_reward)
             apple_list_training.append(env.apple_count)
             timestep_list_training.append(timestep)
             # Saving weights
             agent.save("billodal_syadavil_akhilshr_project_ddqn_1.h5")
             break
+
     if len(agent.memory) > batch_size:
         agent.replay()
-    
+
     eps_list.append(agent.epsilon)
+
     if len(agent.memory) > 60000:
-        if not reached: # THIS WILL RUN ONLY ONCE
+        if not reached:  # THIS WILL RUN ONLY ONCE
             train_start_at = e
             reached = True
         if agent.epsilon > agent.epsilon_min:
-            agent.epsilon = agent.epsilon*((agent.epsilon_min/1)**(1/(EPISODES-train_start_at)))
+            agent.epsilon = agent.epsilon * ((agent.epsilon_min / 1) ** (1 / (EPISODES - train_start_at)))
 
 env.close()
+wandb.finish()
+
